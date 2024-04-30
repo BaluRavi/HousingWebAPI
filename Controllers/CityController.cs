0f50823dd779abbc926646a4dc3362f6ed8d982d
Balu Ravi <balu89.rs@gmail.com>
2024-04-30 09:16:23 +0530

    Add and Delete City Functionality Implemented

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -1,5 +1,7 @@
 ﻿using HousingWebAPI.Data;
+using HousingWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HousingWebAPI.Controllers
 {
@@ -15,19 +17,54 @@ namespace HousingWebAPI.Controllers
         }
 
         [HttpGet]
-        public ActionResult Get()
+        public async Task<ActionResult> Get()
         {
-            var cities = appDataContext.Cities;
+            var cities = await appDataContext.Cities.ToListAsync();
+
             return Ok(cities);
         }
 
-        //test method to pass get method with paramaters
+        // To add new city in the database
 
-        [HttpGet("{id}")]
-        public string Get(int id)
+        //api/City/add?cityname=Chennai
+
+        [HttpPost("add")]
+        [HttpPost("add/{cityName}")]
+        public async Task<IActionResult> AddCity(string cityName)
         {
-            return "Chennai";
+            City city = new City();
+            city.Name = cityName;
+
+            await appDataContext.Cities.AddAsync(city);
+            await appDataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            await appDataContext.Cities.AddAsync(city);
+            await appDataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        //api/City/delete/1
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await appDataContext.Cities.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (city != null)
+            {
+                appDataContext.Cities.Remove(city);
+                await appDataContext.SaveChangesAsync();
+                return Ok(id);
+            }
+            else
+            {
+                return BadRequest("Cant able to delete city");
+            }
         }
     }
-
 }
