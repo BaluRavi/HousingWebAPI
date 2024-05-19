7b4a23d13240fa8196a2a1a1c3749d5dbfdd43c4
Balu Ravi <balu89.rs@gmail.com>
2024-05-19 18:04:09 +0530

    ApiError Class added to Exception Middleware

---
+++ Errors/ApiError.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace HousingWebAPI.Errors
+{
+    public class ApiError
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorDetails { get; set; }
+
+        public ApiError(int errorCode, string errorMessage, string? errorDetails = null)
+        {
+            this.ErrorCode = errorCode;
+            this.ErrorMessage = errorMessage;
+            this.ErrorDetails = errorDetails;
+        }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
--- Middlewares/ExceptionMiddleware.cs
+++ Middlewares/ExceptionMiddleware.cs
@@ -1,14 +1,19 @@
-﻿namespace HousingWebAPI.Middlewares
+﻿using HousingWebAPI.Errors;
+using System.Net;
+
+namespace HousingWebAPI.Middlewares
 {
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate next;
         private readonly ILogger<ExceptionMiddleware> logger;
+        private readonly IWebHostEnvironment hostingEnvironment;
 
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IWebHostEnvironment hostingEnvironment)
         {
             this.next = next;
             this.logger = logger;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
         public async Task Invoke(HttpContext httpContext)
@@ -19,9 +24,35 @@
             }
             catch (Exception ex)
             {
+                ApiError apiError;
+                HttpStatusCode statusCode;
+                string message = string.Empty;
+                var exceptionType = ex.GetType();
+
+                if (exceptionType == typeof(UnauthorizedAccessException))
+                {
+                    statusCode = HttpStatusCode.Forbidden;
+                    message = "You are not authorized";
+                }
+
+                else
+                {
+                    statusCode = HttpStatusCode.InternalServerError;
+                    message = "Some Unknown Error Occured";
+                }
+
+                if (hostingEnvironment.IsDevelopment())
+                {
+                    apiError = new ApiError((int)statusCode, ex.Message, ex.StackTrace.ToString());
+                }
+                else
+                {
+                    apiError = new ApiError((int)statusCode, message);
+                }
                 logger.LogError(ex, ex.Message);
-                httpContext.Response.StatusCode = 500;
-                await httpContext.Response.WriteAsync(ex.Message.ToString());
+                httpContext.Response.StatusCode = (int)statusCode;
+                httpContext.Response.ContentType = "application/json";
+                await httpContext.Response.WriteAsync(apiError.ToString());
             }
         }
     }
